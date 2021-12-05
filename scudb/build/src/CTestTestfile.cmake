32c78b34a00143fe5c1dfc21225e499fc6a74dd5
BruceScu <bruce_scu@163.com>
2021-12-05 20:36:35 +0800

    first commit

---
+++ scudb/build/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/bruce/scudb/src
+# Build directory: /home/bruce/scudb/build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
